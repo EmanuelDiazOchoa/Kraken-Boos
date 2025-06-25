d9d0a68c21d3b71680c21fa54b446e92cfdafa89
emanuel diaz ochoa <emagnr93@gmail.com>
2025-06-24 22:31:37 -0300

    Feat: progresando 1

--- src/pages/EmployeesPage.jsx
+++ src/pages/EmployeesPage.jsx
@@ -1,3 +1,3 @@
-export default function UsersPage() {
-  return <h1 className="text-3xl font-bold">Gestión de Usuarios</h1>;
+export default function EmployessPage() {
+  return <h1 className="text-3xl font-bold">Gestión de Empleados</h1>;
 }
