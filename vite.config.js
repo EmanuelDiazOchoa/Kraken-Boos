e1b69cbe2f24c3869457186157d6c401469097bc
EmanuelDiazOchoa <emagnr93@gmail.com>
2025-07-01 18:39:00 -0300

    Fix: resolviendo fallos

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// Solución para __dirname en ESM
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
